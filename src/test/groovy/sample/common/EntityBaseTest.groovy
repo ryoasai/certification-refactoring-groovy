6f0b46da8bb1ebcb229f56617c575c67f7ac803b
ryoasai <ryoasai74@gmail.com>
2011-03-06 21:22:08 +0900

    単体試験追加 toArrayメソッド簡易化

---
+++ src/test/groovy/sample/common/EntityBaseTest.groovy
@@ -0,0 +1,63 @@
+package sample.common
+
+import org.junit.Before
+import org.junit.Test
+
+import static org.junit.Assert.assertFalse
+import static org.junit.Assert.assertTrue
+import static org.junit.Assert.assertArrayEquals
+import static junit.framework.Assert.assertEquals
+
+public class EntityBaseTest {
+
+	SampleEntity sample = new SampleEntity()
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	void logicalDelete() {
+		assertFalse(sample.isLogicalDeleted())
+
+		sample.logicalDelete()
+		assertTrue(sample.isLogicalDeleted())
+	}
+
+	@Test(expected=IllegalStateException.class)
+	void logicalDelete_AlreadyDeleted() {
+		assertFalse(sample.isLogicalDeleted())
+
+		sample.logicalDelete()
+		
+		// 既に論理削除済みの場合例外となる。
+		sample.logicalDelete()
+	}
+
+    @Test
+    void toArray() {
+        sample.id = 1L
+        sample.name = 'test'
+        sample.age = '30'
+        sample.preCreate()
+
+        def array = sample.toArray()
+        println array
+
+        assertEquals('1', array[0])
+        assertEquals('test', array[1])
+        assertEquals('30', array[2])
+    }
+
+    @Test
+    void fromArray() {
+
+        sample.fromArray(['3', 'test', '50'] as String[])
+        println sample
+
+        assertEquals(3L, sample.id)
+        assertEquals('test', sample.name)
+        assertEquals('50', sample.age)
+    }
+
+}
---
+++ src/test/groovy/sample/common/SampleEntity.groovy
@@ -0,0 +1,33 @@
+package sample.common;
+
+import sample.common.entity.EntityBase
+
+public class SampleEntity extends EntityBase<Long> {
+
+	String name
+	String age
+	
+	@Override
+	public String[] toArray() {
+			
+		def dataList = [
+			id,
+			name,
+			age
+		]
+
+		dataList += createDateColumns()
+	}
+
+	@Override
+	public void fromArray(String[] data) {
+		int i = 0
+
+		id = data[i++]?.toLong()
+		name =data[i++]
+		age = data[i++]
+		
+		readMetaCulumns(data, i)
+	}
+
+}
